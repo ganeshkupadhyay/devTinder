b3b2d2a60fc73d0ff044f928b3c8f1abe53f26c7
Ganesh Kumar Upadhyay <gkupadhyay2002@gmail.com>
2025-06-07 12:34:39 +0530

    writinfg auth middleware

--- src/app.js
+++ src/app.js
@@ -1,54 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Ganesh" , lastName:"upadhyay"});
+const{ adminAuth } =  require("./middleware/auth");
+
+
+
+//handle Auth Middleware for all request GET,POST ,PUT and DELETE
+app.use("/admin",adminAuth);
+
+
+
+app.get("/admin/getAllData",(req,res)=>{
+
+    res.send("All Data Sent ");
 });
 
 
-app.post("/user",(req,res,next)=>{
-    //logic to save data to databse.
-    console.log("1st route handler with no response");
-    next();
-},
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("deleted  a user");
+});
 
-(req,res,next)=>{
-    console.log("2nd route handler with no response");
-    next();
-},
-
-(req,res,next)=>{
-    console.log("3rd route handler with no response");
-    next();
-},
-
-(req, res,next)=>{
-    res.send("finally found the response in 4th route handler");
-}
-
-
-
-
-
-
-);
-
-app.delete("/user",(req,res)=>{
-    res.send("User deleted");
+app.post("/user/login",(req,res)=>{
+    res.send("user logged in successfully");
 })
-
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello hello ");
-})
-
-app.use("/test",(req,res)=>{
-    res.send("hello from test");
-})
-
-
-
-
-
 app.listen(3000,()=>{
     console.log("successfully running the server ");
 });
---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const adminAuth = (req,res,next)=>{
+
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized)
+    {
+        res.status(401).send("Unauthorized user");
+       
+    }
+    else{
+        next();
+    }
+};
+
+
+module.exports = {
+    adminAuth,
+};
